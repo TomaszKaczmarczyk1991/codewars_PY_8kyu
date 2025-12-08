2625c81f042d822278abc83afe08fabac6d72a9c
Tomasz Kaczmarczyk <tomaszkaczmarczyk@tuta.io>
2025-12-09 00:51:04 +0100

    Replace conditional statement with dict lookup

--- switch_it_up.py
+++ switch_it_up.py
@@ -1,21 +1,3 @@
 def switch_it_up(number):
-    if number == 0:
-        return "Zero"
-    elif number == 1:
-        return "One"
-    elif number == 2:
-        return "Two"
-    elif number == 3:
-        return "Three" 
-    elif number == 4:
-        return "Four"
-    elif number == 5:
-        return "Five"
-    elif number == 6:
-        return "Six"
-    elif number == 7:
-        return "Seven"
-    elif number == 8:
-        return "Eight"
-    elif number == 9:
-        return "Nine"
+    converter = {0: "Zero", 1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"}
+    return converter[number]
