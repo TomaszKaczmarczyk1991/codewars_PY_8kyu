0da2367dbd66d1218a776ed837262180eeed7357
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2026-01-03 22:09:21 +0100

    add solution using *args

--- combine_strings.py
+++ combine_strings.py
@@ -1,3 +1,7 @@
 # Create the combine_names function here
 def combine_names(first_name, last_name):
     return first_name + ' ' + last_name
+
+
+# def combine_names(*args):
+#     return ' '.join(args)
