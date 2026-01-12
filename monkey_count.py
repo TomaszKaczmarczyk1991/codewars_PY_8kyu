f93770ddfbb52eae715abe54710918f2f1c7e266
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2026-01-12 14:22:57 +0100

    refactor: simplify monkey_count implementation

--- monkey_count.py
+++ monkey_count.py
@@ -1,8 +1,12 @@
+# def monkey_count(n):
+#     arr = []
+#     r = range(1,n + 1,1)
+#     for x in r:
+#         arr.append(x)
+#     return arr
+
+
 def monkey_count(n):
-    arr = []
-    r = range(1,n + 1,1)
-    for x in r:
-        arr.append(x)
-    return arr
+    return list(range(1, n + 1))
 
 print(monkey_count(5)) # [1, 2, 3, 4, 5]
