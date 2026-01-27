e83d514ffc687bb8fbbb867fc368a5c264a07d66
Tomasz Kaczmarczyk <tomaszkaczmarczyk@tuta.io>
2026-01-27 18:39:25 +0100

    refactor to use any()

--- nessie.py
+++ nessie.py
@@ -1,6 +1,10 @@
+# def is_loch_ness_monster(string):
+#     if ('tree fiddy' in string or
+#         'three fifty' in string or
+#         '3.50' in string):
+#         return True
+#     return False
+
+
 def is_loch_ness_monster(string):
-    if ('tree fiddy' in string or
-        'three fifty' in string or
-        '3.50' in string):
-        return True
-    return False
+    return any(x in string for x in ['tree fiddy', 'three fifty', '3.50'])
