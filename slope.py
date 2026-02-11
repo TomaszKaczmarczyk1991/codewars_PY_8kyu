fad162654d29ec025fcd688e9bc6d5b20d1e49a3
Tomasz Kaczmarczyk <tomaszkaczmarczyk@tuta.io>
2026-02-11 07:26:21 +0100

    add slope.py

---
+++ slope.py
@@ -0,0 +1,10 @@
+import math
+
+def find_slope(points):
+    x = points[2] - points[0]
+    y = points[3] - points[1]
+    
+    if x == 0:
+        return "undefined"
+    
+    return str(int(y / x))
