ab10361675b941ffe57ce2fd773b736d0b242887
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2026-03-14 03:55:10 +0100

    simplify using range()

--- reverse_seq.py
+++ reverse_seq.py
@@ -1,9 +1,4 @@
 def reverse_seq(n):
-    seq = []
-
-    while n >= 1:
-        seq.append(n)
-        n -= 1
-    return seq
+    return list(range(n, 0, -1))
 
 print(reverse_seq(5)) # [5, 4, 3, 2, 1]
